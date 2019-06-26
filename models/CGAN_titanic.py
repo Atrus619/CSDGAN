Fixed the gradients, but now discriminator is demolishing generator. Need to also fix up visualization of outputs to better understand what is happening.

models/CGAN_titanic.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch
-from models.NetUtils import NetUtils
+from models.NetUtils import NetUtils, CustomCatGANLayer
 import torch.optim as optim
 
 
@@ -11,21 +11,18 @@ class CGAN_Generator(nn.Module, NetUtils):
         self.device = device
         self.loss = None
         self.D_G_z2 = None
-        self.le_dict = le_dict
+        self.CCGL = CustomCatGANLayer(cat_mask=cat_mask, le_dict=le_dict)
+        self.out_dim = out_dim
         # self.fixed_noise = torch.randn(bs, nz, device=self.device)
-
-        # Masks
-        self.cat = torch.Tensor(cat_mask).nonzero()
-        self.cont = torch.Tensor(~cat_mask).nonzero()
 
         # Layers
         self.fc1 = nn.Linear(nz + nc, H, bias=True)
         # self.fc1_bn = nn.BatchNorm1d(H)
         self.fc2 = nn.Linear(H, H, bias=True)
         self.fc3 = nn.Linear(H, H, bias=True)
-        self.output = nn.Linear(H, out_dim, bias=True)
+        self.output = nn.Linear(H, self.out_dim, bias=True)
         self.act = nn.LeakyReLU(0.2)
-        self.sm = nn.Softmax(dim=-1)
+        self.sm = nn.Softmax(dim=-2)
 
         # Loss and Optimizer
         # TODO: Try Wasserstein distance instead of BCE Loss
@@ -53,25 +50,8 @@ class CGAN_Generator(nn.Module, NetUtils):
         x = self.act(self.fc2(x))
         x = self.act(self.fc3(x))
         x = self.output(x)
-        return self.output_processing(x)
-
-    def output_processing(self, input_layer):
-        """
-        TODO: Softmax for each categorical variable - https://medium.com/jungle-book/towards-data-set-augmentation-with-gans-9dd64e9628e6
-        :param input_layer: fully connected input layer with size out_dim
-        :return: output of forward pass
-        """
-        cont = input_layer[:, self.cont].squeeze()
-
-        cat = input_layer[:, self.cat].squeeze()
-        catted = torch.empty_like(cat)
-        curr = 0
-        for _, le in self.le_dict.items():
-            newcurr = curr + len(le.classes_)
-            catted[:, curr:newcurr] = self.sm(cat[:, curr:newcurr])
-            curr = newcurr
-
-        return torch.cat([catted, cont], 1)
+        x = self.CCGL(x)
+        return x.view(-1, self.out_dim)
 
     def train_one_step(self, output, label):
         self.zero_grad()

models/NetUtils.py
@@ -1,4 +1,5 @@
-import torch as nn
+import torch.nn as nn
+import torch
 
 
 # Contains utils to be inherited by other nets in this project
@@ -59,3 +60,33 @@ class NetUtils:
         elif classname.find('BatchNorm') != -1:
             nn.init.normal_(m.weight.data, 1.0, 0.02)
             nn.init.constant_(m.bias.data, 0)
+
+
+class CustomCatGANLayer(nn.Module):
+    def __init__(self, cat_mask, le_dict):
+        super(CustomCatGANLayer, self).__init__()
+        # Softmax activation
+        self.sm = nn.Softmax(dim=-2)
+        # Masks
+        self.cat = torch.Tensor(cat_mask).nonzero()
+        self.cont = torch.Tensor(~cat_mask).nonzero()
+        # Label encoding dictionary
+        self.le_dict = le_dict
+
+    def forward(self, input_layer):
+        """
+        Softmax for each categorical variable - https://medium.com/jungle-book/towards-data-set-augmentation-with-gans-9dd64e9628e6
+        :param input_layer: fully connected input layer with size out_dim
+        :return: output of forward pass
+        """
+        cont = input_layer[:, self.cont]
+
+        cat = input_layer[:, self.cat]
+        catted = torch.empty_like(cat)
+        curr = 0
+        for _, le in self.le_dict.items():
+            newcurr = curr + len(le.classes_)
+            catted[:, curr:newcurr] = self.sm(cat[:, curr:newcurr])
+            curr = newcurr
+
+        return torch.cat([catted, cont], 1)

scripts/Utils/utils.py
@@ -36,7 +36,7 @@ def gen_fake_data(netG, bs, nz, nc, labels_list, device):
 
 
 # Train a model on fake data and evaluate on test data in order to evaluate network as it trains
-def evaluate_training_progress(test_range, fake_bs, nz, nc, out_dim, netG, x_test, y_test, manualSeed, labels_list, param_grid, device):
+def evaluate_training_progress(test_range, fake_bs, nz, nc, out_dim, netG, x_test, y_test, manualSeed, labels_list, param_grid, device, le_dict=None):
     fake_scores = []
     fake_models = []
     for size in test_range:
@@ -49,6 +49,8 @@ def evaluate_training_progress(test_range, fake_bs, nz, nc, out_dim, netG, x_tes
             rem -= curr_size
             genned_data = np.concatenate((genned_data, fake_data))
             genned_labels = np.concatenate((genned_labels, output_labels))
+        if le_dict is not None:
+            genned_data = process_fake_output(genned_data, le_dict)
         model_fake_tmp, score_fake_tmp = train_test_logistic_reg(x_train=genned_data, y_train=genned_labels, x_test=x_test, y_test=y_test,
                                                                  param_grid=param_grid, cv=5, random_state=manualSeed, labels=labels_list, verbose=0)
         fake_models.append(model_fake_tmp)
@@ -121,11 +123,7 @@ def gen_labels(size, num_classes, labels_list):
 
 
 # Plots scatter matrix of data set (real or fake)
-def plot_scatter_matrix(X, title, og_df, scaler=None, cont_inputs=None, save=None):
-    if cont_inputs:
-        X_mask = np.array([x in cont_inputs for x in og_df.columns])
-        X = np.array(X)[:, X_mask]
-        og_df = og_df.iloc[:, X_mask]
+def plot_scatter_matrix(X, title, og_df, scaler=None, save=None):
     if scaler:
         X = scaler.inverse_transform(X)
     pd.plotting.scatter_matrix(pd.DataFrame(X, columns=og_df.columns), figsize=(12, 12))
@@ -423,11 +421,11 @@ def create_preprocessed_cat_mask(le_dict, x_train):
 
 def process_fake_output(raw_fake_output, le_dict):
     curr = 0
-    new_fake_output = torch.empty_like(raw_fake_output)
+    new_fake_output = np.copy(raw_fake_output)
     for _, le in le_dict.items():
         n = len(le.classes_)
         newcurr = curr + n
-        max_idx = torch.argmax(raw_fake_output[:, curr:newcurr], 1)
-        new_fake_output[:, curr:newcurr] = torch.eye(n)[max_idx]
+        max_idx = np.argmax(raw_fake_output[:, curr:newcurr], 1)
+        new_fake_output[:, curr:newcurr] = np.eye(n)[max_idx]
         curr = newcurr
     return new_fake_output