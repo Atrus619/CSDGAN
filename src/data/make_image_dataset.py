9709d5ee4e0ab6b95b7aa3bab1e2533193835563
Atrus619 <aj.gray619@gmail.com>
2019-07-23 07:18:30 -0500

    Implement make dataset src code

--- src/data/make_image_dataset.py
+++ src/data/make_image_dataset.py
@@ -13,20 +13,22 @@ This file accomplishes the following:
     3. Unzips files
     4. Splits data into train/val/test splits via stratified sampling and moves into corresponding folders
     5. Deletes original unzipped images
-    6. Pickles label encoder, one hot encoder, and resulting image size
+    6. Pickles label encoder, one hot encoder, resulting image size, and all three generators
 """
 
 # Intake arguments
 parser = argparse.ArgumentParser('Process an image data set')
 
-parser.add_argument('-ip', type=str, help='enter path to image zip', nargs=1, dest='img_zip')
+parser.add_argument('-id', type=str, help='enter identifier for current run', nargs=1, dest='id')
+parser.add_argument('-p', type=str, help='enter path to image zip', nargs=1, dest='path')
 parser.add_argument('-bs', type=int, help='enter batch size for loading', nargs=1, dest='bs')
 parser.add_argument('--x_dim', type=tuple, help="enter desired image size, otherwise first image's size will be used", nargs=1, dest='x_dim')
 parser.add_argument('--splits', type=tuple, help="enter desired train/val/test splits", nargs=1, dest='splits')
 
 args = parser.parse_args()
 
-img_zip = args.img_zip[0]
+id = args.id[0]
+path = args.path[0]
 bs = args.bs[0]
 x_dim = args.x_dim[0] if args.x_dim is not None else None
 splits = args.splits if args.splits is not None else None
@@ -35,19 +37,19 @@ splits = args.splits if args.splits is not None else None
 # bs = 128
 # x_dim = None
 
-assert os.path.splitext(img_zip)[1] == '.zip', "Image file path passed is not zip"
+assert os.path.splitext(path)[1] == '.zip', "Image file path passed is not zip"
 
 # Create directory for downloads
-data_dir = 'downloads/testing'
+data_dir = os.path.join('downloads', id)
 safe_mkdir(data_dir)
 
 # Unzip folders
-zip_ref = zipfile.ZipFile(img_zip, 'r')
+zip_ref = zipfile.ZipFile(path, 'r')
 zip_ref.extractall(data_dir)
 zip_ref.close()
 
 # Preprocess data
-unprocessed_img_path = os.path.join(data_dir, os.path.splitext(os.path.basename(img_zip))[0])
+unprocessed_img_path = os.path.join(data_dir, os.path.splitext(os.path.basename(path))[0])
 assert os.path.exists(unprocessed_img_path), \
     "Image folder not named the same as zip file"
 assert all([os.path.isdir(os.path.join(unprocessed_img_path, x)) for x in os.listdir(unprocessed_img_path)]), \
@@ -58,15 +60,29 @@ import_gen = import_dataset(path=unprocessed_img_path, bs=bs, shuffle=False)
 le, ohe, x_dim = preprocess_imported_dataset(path=unprocessed_img_path, import_gen=import_gen,
                                              splits=splits, x_dim=x_dim)
 
+# Create data loader for each component of data set
+train_gen = import_dataset(os.path.join(unprocessed_img_path, 'train'), bs=bs, shuffle=True)
+val_gen = import_dataset(os.path.join(unprocessed_img_path, 'val'), bs=bs, shuffle=False)
+test_gen = import_dataset(os.path.join(unprocessed_img_path, 'test'), bs=bs, shuffle=False)
+
 # Pickle relevant objects
 model_objects_path = os.path.join(data_dir, "model_objects")
 safe_mkdir(model_objects_path)
 
-with open(os.path.join(model_objects_path, "le.pkl", "wb")) as f:
+with open(os.path.join(model_objects_path, "le.pkl"), "wb") as f:
     pkl.dump(le, f)
 
-with open(os.path.join(model_objects_path, "ohe.pkl", "wb")) as f:
+with open(os.path.join(model_objects_path, "ohe.pkl"), "wb") as f:
     pkl.dump(le, ohe)
 
-with open(os.path.join(model_objects_path, "x_dim.pkl", "wb")) as f:
+with open(os.path.join(model_objects_path, "x_dim.pkl"), "wb") as f:
     pkl.dump(le, x_dim)
+
+with open(os.path.join(model_objects_path, "train_gen.pkl"), "wb") as f:
+    pkl.dump(le, train_gen)
+
+with open(os.path.join(model_objects_path, "val_gen.pkl"), "wb") as f:
+    pkl.dump(le, val_gen)
+
+with open(os.path.join(model_objects_path, "test_gen.pkl"), "wb") as f:
+    pkl.dump(le, test_gen)
--- src/data/make_tabular_dataset.py
+++ src/data/make_tabular_dataset.py
@@ -1,9 +1,73 @@
-# TODO: Add argparser to specify data set and call data_loading.py functions
-import pandas as pd
-from utils.data_loading import load_raw_dataset
+import argparse
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+from utils.data_loading import *
+from classes.Tabular.TabularDataset import TabularDataset
 
-wine = load_raw_dataset('wine')
-wine.to_csv('downloads/wine/processed/wine.csv', index=False)
+"""
+Requirements of data set is that it is contained in a flat file and the continuous vs. categorical vs. integer vs. dependent
+variables are specified. It should also be specified how to deal with missing data (stretch goal).
+"""
 
-iris = load_raw_dataset('iris')
-iris.to_csv('downloads/iris/processed/iris.csv', index=False)
+# Intake arguments
+parser = argparse.ArgumentParser('Process a tabular data set')
+
+parser.add_argument('-id', type=str, help='enter identifier for current run', nargs=1, dest='id')
+parser.add_argument('-p', type=str, help='enter path to flat file', nargs=1, dest='path')
+parser.add_argument('-bs', type=int, help='enter batch size for loading', nargs=1, dest='bs')
+parser.add_argument('-dep_var', type=str, help="enter name of dependent variable", nargs=1, dest='dep_var')
+parser.add_argument('-cont_inputs', type=str, help="enter list of names of continuous inputs", nargs='+', dest='cont_inputs')
+parser.add_argument('-int_inputs', type=str, help="enter list of names of integer inputs", nargs='+', dest='int_inputs')
+parser.add_argument('--test_size', type=int, help="enter desired size of test set", nargs=1, dest='test_size')
+
+args = parser.parse_args()
+
+id = args.id[0]
+path = args.path[0]
+bs = args.bs[0]
+dep_var = args.dep_var[0]
+cont_inputs = args.cont_inputs
+int_inputs = args.int_inputs
+test_size = args.test_size[0] if args.test_size is not None else None
+
+print(path)
+print(bs)
+print(dep_var)
+print(cont_inputs)
+print(int_inputs)
+print(test_size)
+
+# Create directory for downloads
+data_dir = os.path.join('downloads', id)
+safe_mkdir(data_dir)
+
+# Perform various checks and load in data
+assert os.path.splitext(path)[1] in {'.txt', '.csv', '.zip'}, "Path is not zip or flat file"
+if os.path.splitext(path)[1] == '.zip':
+    zip_ref = zipfile.ZipFile(path, 'r')
+    zip_ref.extractall(data_dir)
+    zip_ref.close()
+
+    unzipped_path = os.path.join(data_dir, os.path.splitext(os.path.basename(path))[0])
+    assert os.path.exists(unzipped_path), \
+        "Flat file in zip not named the same as zip file"
+    assert os.path.splitext(data_dir)[1] in {'.txt', '.csv'}, \
+        "Flat file in zip should be .txt or .csv"
+    data = pd.read_csv(data_dir, header=0)
+else:
+    data = pd.read_csv(path, header=0)
+
+# Initialize data set object
+dataset = TabularDataset(df=data,
+                         dep_var=dep_var,
+                         cont_inputs=cont_inputs,
+                         int_inputs=int_inputs,
+                         test_size=test_size)
+
+# Pickle relevant objects
+model_objects_path = os.path.join(data_dir, "model_objects")
+safe_mkdir(model_objects_path)
+
+with open(os.path.join(model_objects_path, "dataset.pkl"), "wb") as f:
+    pkl.dump(dataset, f)
