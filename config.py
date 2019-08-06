a7848b02dbec8dcdad857b6e7781f4eaf8d5a389
Atrus619 <aj.gray619@gmail.com>
2019-08-05 22:49:23 -0500

    Fixed docker run. Wrote docker-compose. DB connection not working.

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
     DATABASE_URL = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'instance', 'csdgan.sqlite')
-    MYSQL_DATABASE_HOST = os.environ.get('DB_HOST')
+    MYSQL_DATABASE_HOST = os.environ.get('DB_HOST') or 'localhost'
     MYSQL_DATABASE_USER = 'csdgan'
     MYSQL_DATABASE_PASSWORD = os.environ.get('DB_PW') or 'you-might-guess-this-time'
     MYSQL_DATABASE_DB = 'csdgan'
